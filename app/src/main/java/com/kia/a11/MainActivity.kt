49e876f4facaf35f4baf68ae02f0238f3f6e3110
Your Name <you@example.com>
2021-03-07 03:16:12 -0800

    BottomNavigation

--- app/src/main/java/com/kia/a11/MainActivity.kt
+++ app/src/main/java/com/kia/a11/MainActivity.kt
@@ -9,13 +9,16 @@ import androidx.navigation.NavGraph
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +27,14 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)as NavHostFragment
         navController= navHostFragment.findNavController()
 
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.homeFragment, R.id.searchFragment)
+        )
+
         setSupportActionBar(toolbar)
-        setupActionBarWithNavController(navController)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+        bottom_nav.setupWithNavController(navController)
 
     }
 
