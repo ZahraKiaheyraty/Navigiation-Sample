d042b0d276fc0a5134250d95d286969e11f3bc1b
Your Name <you@example.com>
2021-03-07 06:30:37 -0800

    DeepLink

--- app/src/main/java/com/kia/a11/LoginFragment.kt
+++ app/src/main/java/com/kia/a11/LoginFragment.kt
@@ -6,12 +6,20 @@ import android.os.Parcelable
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import kotlinx.android.synthetic.main.frafment_login.*
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class LoginFragment: Fragment(R.layout.frafment_login) {
+
+    private val args:LoginFragmentArgs by navArgs()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        var usernameDeepLink = args.username
+        edit_text_username.setText(usernameDeepLink)
+
         button_confirm.setOnClickListener {
             val username = edit_text_username.text.toString()
             val password = edit_text_password.text.toString()
